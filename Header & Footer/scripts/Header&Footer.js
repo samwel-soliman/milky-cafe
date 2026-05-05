ae8abf8fb871cdcae08a8b67dddfffc12db2366a
Samwel.soliman <samwelremongerges@gmail.com>
2026-05-05 23:10:46 +0300

    Add files via upload

---
+++ Header & Footer/scripts/Header&Footer.js
@@ -0,0 +1,14 @@
+
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclock = () => {
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+}
